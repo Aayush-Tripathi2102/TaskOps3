3fbdaca7d1ee8887f17a6f79fc686ca59c8e58ae
LakshRJain <jainlaksh2006@gmail.com>
2025-02-23 01:00:27 +0530

    Case Change

--- screens/Sabotage.tsx
+++ screens/Sabotage.tsx
@@ -48,7 +48,7 @@ export default function Sabotage() {
   };
 
   const handleLogout = () => {
-    if (password === "tee") {
+    if (password.toLowerCase() === "tee") {
       setUserId(null);
       setLogoutModal(false);
       ToastAndroid.show("Logged Out Successfully!", ToastAndroid.SHORT);
